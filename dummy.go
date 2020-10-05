5b7753060809e6d96070bc33d5b69d04329ab3db
Hedzr Yeh <hedzrz@gmail.com>
2020-10-05 09:17:24 +0800

    improved fatal function impl

--- dummy.go
+++ dummy.go
@@ -4,7 +4,7 @@ package log
 
 import (
 	"fmt"
-	"log"
+	"os"
 )
 
 // NewDummyLogger return a dummy logger
@@ -12,116 +12,43 @@ func NewDummyLogger() Logger {
 	return &dummyLogger{}
 }
 
-// NewStdLogger return a stdlib `log` logger
-func NewStdLogger() Logger {
-	return &stdLogger{Level: InfoLevel}
-}
-
-// NewStdLoggerWith return a stdlib `log` logger
-func NewStdLoggerWith(lvl Level) Logger {
-	return &stdLogger{Level: lvl}
-}
-
 type dummyLogger struct{}
 
-func (d *dummyLogger) Tracef(msg string, args ...interface{}) {
-	//panic("implement me")
+func (d *dummyLogger) Trace(args ...interface{}) {}
+func (d *dummyLogger) Debug(args ...interface{}) {}
+func (d *dummyLogger) Info(args ...interface{})  {}
+func (d *dummyLogger) Warn(args ...interface{})  {}
+func (d *dummyLogger) Error(args ...interface{}) {}
+
+func (d *dummyLogger) Fatal(args ...interface{}) {
+	if InTesting() {
+		panic(fmt.Sprint(args...))
+	}
+	os.Exit(1)
 }
 
-func (d *dummyLogger) Debugf(msg string, args ...interface{}) {
-	//panic("implement me")
-}
-
-func (d *dummyLogger) Infof(msg string, args ...interface{}) {
-	//panic("implement me")
-}
-
-func (d *dummyLogger) Warnf(msg string, args ...interface{}) {
-	//panic("implement me")
-}
-
-func (d *dummyLogger) Errorf(msg string, args ...interface{}) {
-	//panic("implement me")
-}
+func (d *dummyLogger) Panic(args ...interface{})              { panic(fmt.Sprint(args...)) }
+func (d *dummyLogger) Print(args ...interface{})              {}
+func (d *dummyLogger) Println(args ...interface{})            {}
+func (d *dummyLogger) Tracef(msg string, args ...interface{}) {}
+func (d *dummyLogger) Debugf(msg string, args ...interface{}) {}
+func (d *dummyLogger) Infof(msg string, args ...interface{})  {}
+func (d *dummyLogger) Warnf(msg string, args ...interface{})  {}
+func (d *dummyLogger) Errorf(msg string, args ...interface{}) {}
 
 func (d *dummyLogger) Fatalf(msg string, args ...interface{}) {
 	//panic("implement me")
-}
-
-func (d *dummyLogger) Panicf(msg string, args ...interface{}) {
-	panic(fmt.Sprintf(msg, args...))
-}
-
-func (d *dummyLogger) Printf(msg string, args ...interface{}) {
-	//panic("implement me")
-}
-
-func (d *dummyLogger) SetLevel(lvl Level) {
-	//panic("implement me")
-}
-
-func (d *dummyLogger) GetLevel() Level {
-	//panic("implement me")
-	return InfoLevel // logex.GetLevel()
-}
-
-func (d *dummyLogger) Setup() {
-	//panic("implement me")
-}
-
-type stdLogger struct {
-	Level
-}
-
-func (s *stdLogger) Tracef(msg string, args ...interface{}) {
-	if s.Level >= TraceLevel {
-		log.Printf(msg, args...)
+	if InTesting() {
+		panic(fmt.Sprintf(msg, args...))
 	}
+	os.Exit(1)
 }
 
-func (s *stdLogger) Debugf(msg string, args ...interface{}) {
-	if s.Level >= DebugLevel {
-		log.Printf(msg, args...)
-	}
-}
-
-func (s *stdLogger) Infof(msg string, args ...interface{}) {
-	if s.Level >= InfoLevel {
-		log.Printf(msg, args...)
-	}
-}
-
-func (s *stdLogger) Warnf(msg string, args ...interface{}) {
-	log.Printf(msg, args...)
-}
-
-func (s *stdLogger) Errorf(msg string, args ...interface{}) {
-	log.Printf(msg, args...)
-}
-
-func (s *stdLogger) Fatalf(msg string, args ...interface{}) {
-	//log.Fatalf(msg, args...)
-	log.Panicf(msg, args...)
-}
-
-func (s *stdLogger) Panicf(msg string, args ...interface{}) {
-	log.Panicf(msg, args...)
-}
-
-func (s *stdLogger) Printf(msg string, args ...interface{}) {
-	log.Printf(msg, args...)
-}
-
-func (s *stdLogger) SetLevel(lvl Level) {
-	s.Level = lvl
-}
-
-func (s *stdLogger) GetLevel() Level {
-	return s.Level
-}
-
-func (s *stdLogger) Setup() {
-}
+func (d *dummyLogger) Panicf(msg string, args ...interface{}) { panic(fmt.Sprintf(msg, args...)) }
+func (d *dummyLogger) Printf(msg string, args ...interface{}) {}
+func (d *dummyLogger) SetLevel(lvl Level)                     {}
+func (d *dummyLogger) GetLevel() Level                        { return InfoLevel }
+func (d *dummyLogger) Setup()                                 {}
 
 //
 //
--- dummy_test.go
+++ dummy_test.go
@@ -5,29 +5,21 @@ import (
 	"testing"
 )
 
-func TestNewLoggerConfig(t *testing.T) {
-	log := NewDummyLogger()
-	log.Tracef("")
-	log.Debugf("")
-	log.Infof("")
-	log.Warnf("")
-	log.Errorf("")
-	log.Fatalf("")
-	log.Printf("")
-	log.SetLevel(InfoLevel)
-	_ = log.GetLevel()
-	log.Setup()
-	tf(log)
-	tp(log)
-
-	log = NewStdLogger()
+func TestStdLogger(t *testing.T) {
+	log := NewStdLogger()
 	log.SetLevel(TraceLevel)
-	log.Tracef("")
-	log.Debugf("")
-	log.Infof("")
-	log.Warnf("")
-	log.Errorf("")
+	log.Tracef("trace")
+	log.Debugf("debug")
+	log.Infof("info")
+	log.Warnf("warn")
+	log.Errorf("error")
 	// log.Fatalf("")
+	l := AsL(log)
+	l.Trace("trace")
+	l.Debug("debug")
+	l.Info("info")
+	l.Warn("warn")
+	l.Error("error")
 
 	log = NewStdLoggerWith(TraceLevel)
 	log.Printf("")
@@ -38,7 +30,29 @@ func TestNewLoggerConfig(t *testing.T) {
 	tp(log)
 }
 
+func TestNewLoggerConfig(t *testing.T) {
+	log := NewDummyLogger()
+	log.Tracef("")
+	log.Debugf("")
+	log.Infof("")
+	log.Warnf("")
+	log.Errorf("")
+	//log.Fatalf("")
+	log.Printf("")
+	log.SetLevel(InfoLevel)
+	_ = log.GetLevel()
+	log.Setup()
+	tf(log)
+	tp(log)
+
+}
+
 func tf(logger Logger) {
+	tf1(logger)
+	tf2(logger)
+}
+
+func tf1(logger Logger) {
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Printf("caught: %v\n", err)
@@ -48,12 +62,37 @@ func tf(logger Logger) {
 	logger.Fatalf("fatal")
 }
 
-func tp(logger Logger) {
+func tf2(logger Logger) {
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Printf("caught: %v\n", err)
 		}
 	}()
 
-	logger.Panicf("fatal")
+	logger.(LoggerExt).Fatal("fatal")
+}
+
+func tp(logger Logger) {
+	tp1(logger)
+	tp2(logger)
+}
+
+func tp1(logger Logger) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("caught: %v\n", err)
+		}
+	}()
+
+	logger.Panicf("panic")
+}
+
+func tp2(logger Logger) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("caught: %v\n", err)
+		}
+	}()
+
+	logger.(LoggerExt).Panic("panic")
 }
