89c9b6bf091d151b23452de4e40468da6da644fb
Hedzr Yeh <hedzrz@gmail.com>
2021-04-30 16:24:45 +0800

    local checker

--- build.sh
+++ build.sh
@@ -3,9 +3,15 @@
 set -e
 
 cov(){
-	go test -v -race -coverprofile=coverage.txt ./...
-	go tool cover -html=coverage.txt -o cover.html
-	open cover.html
+	for GOOS in darwin linux windows; do
+		go test -v -race -coverprofile=coverage-$GOOS.txt ./...
+		go tool cover -html=coverage-$GOOS.txt -o cover-$GOOS.html
+	done
+	open cover-darwin.html
+}
+
+bf1(){
+	for GOOS in $(go tool dist list|awk -F'/' '{print $1}'|sort -u); do echo -e "\n\nTESTING FOR $GOOS ...\n"; go test -v -race -coverprofile=coverage-$GOOS.txt -test.run=^TestDirTimestamps$ ./dir/ || exit; done
 }
 
 fmt(){
