cc75403f62a022dc32f1e5a21f3ff81a963995d8
hz <hedzrz@gmail.com>
2021-12-03 23:51:28 +0800

    improved: RunCommand; added: RunCommandFull

--- exec/sudo.go
+++ exec/sudo.go
@@ -10,48 +10,67 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"sync"
 	"syscall"
 )
 
 // Run runs an OS command
-func Run(command string, arguments ...string) error {
-	_, _, err := RunCommand(command, false, arguments...)
-	return err
+func Run(command string, arguments ...string) (err error) {
+	_, _, err = RunCommand(command, false, arguments...)
+	return
 }
 
 // Sudo runs an OS command with sudo prefix
-func Sudo(command string, arguments ...string) (int, string, error) {
-	sudocmd, err := exec.LookPath("sudo")
+func Sudo(command string, arguments ...string) (retCode int, stdoutText string, err error) {
+	var sudocmd string
+	sudocmd, err = exec.LookPath("sudo")
 	if err != nil {
 		return -1, "'sudo' not found", Run(command, arguments...)
 	}
 
-	rc, output, err1 := RunCommand(sudocmd, true, append([]string{command}, arguments...)...)
-	return rc, output, err1
+	retCode, stdoutText, err = RunCommand(sudocmd, true, append([]string{command}, arguments...)...)
+	return
 }
 
 // RunWithOutput runs an OS command and collect the result outputting
-func RunWithOutput(command string, arguments ...string) (int, string, error) {
+func RunWithOutput(command string, arguments ...string) (retCode int, stdoutText string, err error) {
 	return RunCommand(command, true, arguments...)
 }
 
-// RunCommand runs an OS command
+// RunCommand runs an OS command and return outputs
 func RunCommand(command string, readStdout bool, arguments ...string) (retCode int, stdoutText string, err error) {
+	var errText string
+	retCode, stdoutText, errText, err = RunCommandFull(command, readStdout, arguments...)
+	if errText != "" {
+		stdoutText += errText
+	}
+	return
+}
+
+// RunCommandFull runs an OS command and return the all outputs
+func RunCommandFull(command string, readStdout bool, arguments ...string) (retCode int, stdoutText, stderrText string, err error) {
 	cmd := exec.Command(command, arguments...)
 
-	var output string
 	var stdout io.ReadCloser
 	var stderr io.ReadCloser
+	var output, slurp bytes.Buffer
+	var wg sync.WaitGroup
 
 	if readStdout {
 		// Connect pipe to read Stdout
 		stdout, err = cmd.StdoutPipe()
 		if err != nil {
 			// Failed to connect pipe
-			return 0, "", fmt.Errorf("%q failed to connect stdout pipe: %v", command, err)
+			return 0, "", "", fmt.Errorf("%q failed to connect stdout pipe: %v", command, err)
 		}
 
 		defer stdout.Close()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, _ = io.Copy(&output, stdout)
+		}()
+
 	} else {
 		cmd.Stdout = os.Stdout
 		//cmd.Stderr = os.Stderr
@@ -61,52 +80,60 @@ func RunCommand(command string, readStdout bool, arguments ...string) (retCode i
 	stderr, err = cmd.StderrPipe()
 	if err != nil {
 		// Failed to connect pipe
-		return 0, "", fmt.Errorf("%q failed to connect stderr pipe: %v", command, err)
+		return 0, "", "", fmt.Errorf("%q failed to connect stderr pipe: %v", command, err)
 	}
 
 	defer stderr.Close()
 
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		_, _ = io.Copy(&slurp, stderr)
+	}()
+
 	// Do not use cmd.Run()
 	if err = cmd.Start(); err != nil {
 		// Problem while copying stdin, stdout, or stderr
-		return 0, "", fmt.Errorf("%q failed: %v", command, err)
+		return 0, "", "", fmt.Errorf("%q failed: %v", command, err)
 	}
 
 	// Zero exit status
 	// Darwin: launchctl can fail with a zero exit status,
 	// so check for emtpy stderr
 	if command == "launchctl" {
-		slurp, _ := ioutil.ReadAll(stderr)
-		if len(slurp) > 0 && !bytes.HasSuffix(slurp, []byte("Operation now in progress\n")) {
-			return 0, "", fmt.Errorf("%q failed with stderr: %s", command, slurp)
+		slurpText, _ := ioutil.ReadAll(stderr)
+		if len(slurpText) > 0 && !bytes.HasSuffix(slurpText, []byte("Operation now in progress\n")) {
+			return 0, "", "", fmt.Errorf("%q failed with stderr: %s", command, slurpText)
 		}
 	}
 
-	slurp, _ := ioutil.ReadAll(stderr)
+	// slurp, _ := ioutil.ReadAll(stderr)
+
+	wg.Wait()
 
 	if err = cmd.Wait(); err != nil {
 		exitStatus, ok := IsExitError(err)
 		if ok {
 			// Command didn't exit with a zero exit status.
-			return exitStatus, output, errors.New("%q failed with stderr:\n%v\n  ", command, string(slurp)).Attach(err)
+			return exitStatus, output.String(), slurp.String(), errors.New("%q failed with stderr:\n%v\n  ", command, slurp.String()).Attach(err)
 		}
 
 		// An error occurred and there is no exit status.
 		//return 0, output, fmt.Errorf("%q failed: %v |\n  stderr: %s", command, err.Error(), slurp)
-		return 0, output, errors.New("%q failed with stderr:\n%v\n  ", command, string(slurp)).Attach(err)
+		return 0, output.String(), slurp.String(), errors.New("%q failed with stderr:\n%v\n  ", command, slurp.String()).Attach(err)
 	}
 
-	if readStdout {
-		var out []byte
-		out, err = ioutil.ReadAll(stdout)
-		if err != nil {
-			return 0, "", fmt.Errorf("%q failed while attempting to read stdout: %v", command, err)
-		} else if len(out) > 0 {
-			output = string(out)
-		}
-	}
+	//if readStdout {
+	//	var out []byte
+	//	out, err = ioutil.ReadAll(stdout)
+	//	if err != nil {
+	//		return 0, "", fmt.Errorf("%q failed while attempting to read stdout: %v", command, err)
+	//	} else if len(out) > 0 {
+	//		output = string(out)
+	//	}
+	//}
 
-	return 0, output, nil
+	return 0, output.String(), slurp.String(), nil
 }
 
 // IsEAccess detects whether err is a EACCESS errno or not
