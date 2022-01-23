de3ea8a368c3f9b78a615f8808cc893c6af87a52
hz <hedzrz@gmail.com>
2022-01-23 11:18:23 +0800

    move LookPath

--- exec/dir.go
+++ exec/dir.go
@@ -9,12 +9,21 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"os/user"
 	"path"
 	"path/filepath"
 	"strings"
 	"syscall"
 )
+
+// LookPath searches for an executable named file in the
+// directories named by the PATH environment variable.
+// If file contains a slash, it is tried directly and the PATH is not consulted.
+// The result may be an absolute path or a path relative to the current directory.
+func LookPath(file string) (string, error) {
+	return exec.LookPath(file)
+}
 
 // GetExecutableDir returns the executable file directory
 // Deprecated see also dir.GetExecutableDir
