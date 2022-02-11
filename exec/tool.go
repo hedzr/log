d98fa452c02b10d8a7e98b5170f50c693db17681
hz <hedzrz@gmail.com>
2022-02-11 13:07:49 +0800

    imp: StripLeftTabs, StripHtmlTags, StripQuotes

--- exec/tool.go
+++ exec/tool.go
@@ -4,10 +4,71 @@ import (
 	"bufio"
 	"fmt"
 	"strings"
+	"unicode/utf8"
 )
 
 func toStringSimple(i interface{}) string {
 	return fmt.Sprintf("%v", i)
+}
+
+// StripLeftTabs removes the padding tabs at left margin.
+// The least tab chars will be erased at left side of lines, and the
+// tab chars beyond the least at left will be kept.
+func StripLeftTabs(s string) string { return stripLeftTabs(s) }
+
+func stripLeftTabs(s string) string {
+
+	var lines []string
+	var tabs int = 1000
+	var emptyLines []int
+	var sb strings.Builder
+	var line int
+	var noLastLF bool = !strings.HasSuffix(s, "\n")
+
+	scanner := bufio.NewScanner(bufio.NewReader(strings.NewReader(s)))
+	for scanner.Scan() {
+		str := scanner.Text()
+		i, n, allTabs := 0, len(str), true
+		for ; i < n; i++ {
+			if str[i] != '\t' {
+				allTabs = false
+				if tabs > i && i > 0 {
+					tabs = i
+					break
+				}
+			}
+		}
+		if i == n && allTabs {
+			emptyLines = append(emptyLines, line)
+		}
+		lines = append(lines, str)
+		line++
+	}
+
+	pad := strings.Repeat("\t", tabs)
+	for i, str := range lines {
+		if strings.HasPrefix(str, pad) {
+			sb.WriteString(str[tabs:])
+		} else if inIntSlice(i, emptyLines) {
+		} else {
+			sb.WriteString(str)
+		}
+		if noLastLF && i == len(lines)-1 {
+			break
+		}
+		sb.WriteRune('\n')
+	}
+
+	return sb.String()
+}
+
+func inIntSlice(i int, slice []int) bool {
+	for _, n := range slice {
+		if n == i {
+			return true
+		}
+	}
+	return false
 }
 
 // LeftPad inserts spaces at beginning of each line
@@ -28,6 +89,60 @@ func leftPad(s string, pad int) string {
 	}
 	return sb.String()
 }
+
+// StripHtmlTags aggressively strips HTML tags from a string.
+// It will only keep anything between `>` and `<`.
+func StripHtmlTags(s string) string { return stripHtmlTags(s) }
+
+const (
+	htmlTagStart = 60 // Unicode `<`
+	htmlTagEnd   = 62 // Unicode `>`
+)
+
+// Aggressively strips HTML tags from a string.
+// It will only keep anything between `>` and `<`.
+func stripHtmlTags(s string) string {
+	// Setup a string builder and allocate enough memory for the new string.
+	var builder strings.Builder
+	builder.Grow(len(s) + utf8.UTFMax)
+
+	in := false // True if we are inside an HTML tag.
+	start := 0  // The index of the previous start tag character `<`
+	end := 0    // The index of the previous end tag character `>`
+
+	for i, c := range s {
+		// If this is the last character and we are not in an HTML tag, save it.
+		if (i+1) == len(s) && end >= start {
+			builder.WriteString(s[end:])
+		}
+
+		// Keep going if the character is not `<` or `>`
+		if c != htmlTagStart && c != htmlTagEnd {
+			continue
+		}
+
+		if c == htmlTagStart {
+			// Only update the start if we are not in a tag.
+			// This make sure we strip out `<<br>` not just `<br>`
+			if !in {
+				start = i
+			}
+			in = true
+
+			// Write the valid string between the close and start of the two tags.
+			builder.WriteString(s[end:start])
+			continue
+		}
+		// else c == htmlTagEnd
+		in = false
+		end = i + 1
+	}
+	s = builder.String()
+	return s
+}
+
+// StripQuotes strips first and last quote char (double quote or single quote).
+func StripQuotes(s string) string { return trimQuotes(s) }
 
 // TrimQuotes strips first and last quote char (double quote or single quote).
 func TrimQuotes(s string) string { return trimQuotes(s) }
