cbec68a8fc4bf1405984afeba6fd17ea6acbba9a
hz <hedzrz@gmail.com>
2023-02-26 19:24:41 +0800

    improved detects package: added InDebugging(), ...
    Signed-off-by: hz <hedzrz@gmail.com>

--- detects/detects.go
+++ detects/detects.go
@@ -1,29 +1,30 @@
 package detects
 
 import (
-	"github.com/hedzr/log/dir"
 	"os"
 	"strings"
+
+	"github.com/hedzr/log/buildtags"
+	"github.com/hedzr/log/dir"
 )
 
 // InK8s detects if the service is running under k8s environment.
 func InK8s() bool {
-	return os.Getenv("KUBERNETES_SERVICE_HOST") != ""
+	return os.Getenv("KUBERNETES_SERVICE_HOST") != "" || buildtags.IsK8sBuild()
 }
 
 // InK8sYN is yet another DetectInK8s impl
 func InK8sYN() bool {
-	return dir.FileExists("/var/run/secrets/kubernetes.io")
+	return dir.FileExists("/var/run/secrets/kubernetes.io") || buildtags.IsK8sBuild()
 }
 
 // InIstio detects if the service is running under istio injected.
 //
-// IMPORTANT
+// # IMPORTANT
 //
 // To make this detector work properly, you must mount a DownwordAPI
 // volume to your container/pod. See also:
 // https://kubernetes.io/en/docs/tasks/inject-data-application/downward-api-volume-expose-pod-information/
-//
 func InIstio() bool {
 	if os.Getenv("KUBERNETES_SERVICE_HOST") != "" {
 		kf := "/etc/podinfo/labels"
@@ -36,10 +37,17 @@ func InIstio() bool {
 			}
 		}
 	}
-	return false
+	return buildtags.IsIstioBuild()
 }
 
 // InDocker detects if the service is running under docker environment.
+//
+// InDocker test these two conditions:
+// 1. find if `/.dockerenv` exists or not.
+// 2. `docker` in buildtags
 func InDocker() bool {
-	return dir.FileExists("/.dockerenv")
+	if dir.FileExists("/.dockerenv") {
+		return true
+	}
+	return buildtags.IsDockerBuild()
 }
---
+++ detects/env.go
@@ -0,0 +1,107 @@
+// Copyright © 2023 Hedzr Yeh.
+
+package detects
+
+import (
+	"os"
+
+	"github.com/hedzr/log"
+	"github.com/hedzr/log/trace"
+)
+
+// InDebugging return the status if cmdr was built with debug mode / or the app running under a debugger attached.
+//
+// To enable the debugger attached mode for cmdr, run `go build` with `-tags=delve` options. eg:
+//
+//	go run -tags=delve ./cli
+//	go build -tags=delve -o my-app ./cli
+//
+// For Goland, you can enable this under 'Run/Debug Configurations', by adding the following into 'Go tool arguments:'
+//
+//	-tags=delve
+//
+// InDebugging() is a synonym to IsDebuggerAttached().
+//
+// NOTE that `isdelve` algor is from https://stackoverflow.com/questions/47879070/how-can-i-see-if-the-goland-debugger-is-running-in-the-program
+//
+// noinspection GoBoolExpressions
+func InDebugging() bool {
+	return log.InDebugging() // isdelve.Enabled
+}
+
+// IsDebuggerAttached return the status if cmdr was built with debug mode / or the app running under a debugger attached.
+//
+// To enable the debugger attached mode for cmdr, run `go build` with `-tags=delve` options. eg:
+//
+//	go run -tags=delve ./cli
+//	go build -tags=delve -o my-app ./cli
+//
+// For Goland, you can enable this under 'Run/Debug Configurations', by adding the following into 'Go tool arguments:'
+//
+//	-tags=delve
+//
+// IsDebuggerAttached() is a synonym to InDebugging().
+//
+// NOTE that `isdelve` algor is from https://stackoverflow.com/questions/47879070/how-can-i-see-if-the-goland-debugger-is-running-in-the-program
+//
+// noinspection GoBoolExpressions
+func IsDebuggerAttached() bool {
+	return log.InDebugging() // isdelve.Enabled
+}
+
+func InTracing() bool {
+	return trace.IsEnabled()
+}
+
+// InTesting detects whether is running under go test mode
+func InTesting() bool {
+	return log.InTestingT(os.Args)
+	// if !strings.HasSuffix(tool.SavedOsArgs[0], ".test") &&
+	//	!strings.Contains(tool.SavedOsArgs[0], "/T/___Test") {
+	//
+	//	// [0] = /var/folders/td/2475l44j4n3dcjhqbmf3p5l40000gq/T/go-build328292371/b001/exe/main
+	//	// !strings.Contains(SavedOsArgs[0], "/T/go-build")
+	//
+	//	for _, s := range tool.SavedOsArgs {
+	//		if s == "-test.v" || s == "-test.run" {
+	//			return true
+	//		}
+	//	}
+	//	return false
+	//
+	// }
+	// return true
+}
+
+// func InTestingWith(osArgs []string) bool { return log.InTestingT(osArgs) } // test if in testing mode
+
+// InDevelopingTime detects whether is in developing time.
+//
+// If the main program has been built as an executable binary, we
+// would assume which is not in developing time.
+//
+// If log.GetDebugMode() is true, that's in developing time too.
+func InDevelopingTime() (status bool) {
+	return InDebugging() || InTesting()
+}
+
+// InDockerEnvSimple detects whether is running within docker
+// container environment.
+//
+// InDockerEnvSimple finds if `/.dockerenv` exists or not.
+func InDockerEnvSimple() (status bool) {
+	return isRunningInDockerContainer()
+}
+
+func isRunningInDockerContainer() bool {
+	// docker creates a .dockerenv file at the root
+	// of the directory tree inside the container.
+	// if this file exists then the viewer is running
+	// from inside a container so return true
+
+	if _, err := os.Stat("/.dockerenv"); err == nil {
+		return true
+	}
+
+	return false
+}
