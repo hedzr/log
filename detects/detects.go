7f9042d52905511e3d0ecd3faab04ff06ec58c68
hz <hedzrz@gmail.com>
2023-03-05 14:32:08 +0800

    added stubs in 'detects' pkg to unify detectors api entry

--- detects/detects.go
+++ detects/detects.go
@@ -1,3 +1,6 @@
+// Package detects collects all detector, buildtags, environment settings in one place.
+//
+// By using detects package, you have no needs to call `buildtags`, `states` and other similar packages.
 package detects
 
 import (
@@ -6,6 +9,9 @@ import (
 
 	"github.com/hedzr/log/buildtags"
 	"github.com/hedzr/log/dir"
+	"github.com/hedzr/log/isdelve"
+	"github.com/hedzr/log/states"
+	"github.com/hedzr/log/trace"
 )
 
 // InK8s detects if the service is running under k8s environment.
@@ -51,3 +57,27 @@ func InDocker() bool {
 	}
 	return buildtags.IsDockerBuild()
 }
+
+func IsDockerBuild() bool { return buildtags.IsDockerBuild() }
+func IsK8sBuild() bool    { return buildtags.IsK8sBuild() }
+func IsIstioBuild() bool  { return buildtags.IsIstioBuild() }
+
+func IsVerboseBuild() bool       { return buildtags.VerboseEnabled } // is verbose build
+func IsVerboseModeEnabled() bool { return Env().IsVerboseMode() }    // is verbose build, or is CLI Verbose mode enabled (by `--verbose`)?
+func GetVerboseLevel() int       { return Env().CountOfVerbose() }   //
+func IsQuietModeEnabled() bool   { return Env().IsQuietMode() }      // is quiet build, or is CLI Quiet mode enabled (by `--verbose`)?
+func GetQuietLevel() int         { return Env().CountOfQuiet() }     //
+func IsNoColorModeEnabled() bool { return Env().IsNoColorMode() }    //
+func GetNoColorLevel() int       { return Env().CountOfNoColor() }   //
+func IsDebugBuild() bool         { return isdelve.Enabled }          // is debug build?
+func IsDebugModeEnabled() bool   { return Env().GetDebugMode() }     // is debug build, or is CLI debug mode enabled (by `--debug`)?
+func GetDebugLevel() int         { return Env().GetDebugLevel() }    //
+func IsTracingEnabled() bool     { return trace.IsEnabled() }        // is tracing-flag true in trace package
+func IsTraceModeEnabled() bool   { return Env().GetTraceMode() }     // is CLI trace mode enabled (by `--trace`)? or is tracing-flag true in trace package
+func GetTraceLevel() int         { return Env().GetTraceLevel() }    //
+
+// States or Env returns a minimal environment settings for a typical CLI app.
+//
+// See also states.CmdrMinimal.
+func States() states.CmdrMinimal { return states.Env() }
+func Env() states.CmdrMinimal    { return states.Env() } // States or Env returns a minimal environment settings for a typical CLI app.
